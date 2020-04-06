2600df3469b7f5aabb47f2124b72d440ca6cf4d8
Vasu Joshi <joshivasu5@gmail.com>
2020-04-06 13:41:45 -0400

    Added client id and secret key
    Testing user loggin with google oAuth 2.0

--- config/config.js
+++ config/config.js
@@ -2,8 +2,8 @@ module.exports = {
     DB_URL: "mongodb+srv://admin:admin@project1-psmml.mongodb.net/todolist?retryWrites=true&w=majority",
     //DB_URL: "mongodb+srv://test:test@cluster0-6kis3.mongodb.net/todolist?retryWrites=true&w=majority",
     'google': {
-        clientID: '',
-        clientSecret: '',
+        clientID: '542012907978-hu44ud0m1hevfor48sakuhg961j7qskb.apps.googleusercontent.com',
+        clientSecret: '_4ui6kEvrpv2VkTOgT2B_nLF',
         callbackURL: 'https://localhost:3000/google/callback'
     },
 }
